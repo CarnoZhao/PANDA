58f074017f7ed939d4b3b4ea814f7d7f2d9c8d41
CarnoZhao <273806108@qq.com>
2020-06-02 17:26:58 +0800

    remove bak, listize

--- ClassifierTrainer.py
+++ ClassifierTrainer.py
@@ -106,25 +106,11 @@ class Train(object):
         elif self.kwargs['nt'] == "resnext50":
             self.net = PoolTileNet.SemiResNextList(n = self.kwargs['nc'])
         elif self.kwargs['nt'] == "eff":
-            self.net = PoolTileNet.MyEfficientNet(self.kwargs['nc'])
+            self.net = PoolTileNet.MyEfficientNetList(self.kwargs['nc'])
         self.net = self.net.cuda()
         if self.kwargs['MD']:
             dic = torch.load(self.kwargs['MD'])
             self.net.load_state_dict(dic)
-
-    def callback_bak(self, i, loss, valloader):
-        self.losses["train"].append(loss)
-        self.net.eval()
-        with torch.no_grad():
-            valloss = 0; valcnt = 0
-            for x, y in tqdm.tqdm(valloader, desc = "Validating...", leave = False, mininterval = 60):
-                x = x.cuda(); y = y.cuda()
-                yhat = self.net(x)
-                cost = self.loss(yhat, y.long())
-                valloss += cost.item() * len(x); valcnt += len(x)
-            self.losses["val"].append(valloss / valcnt)
-        printOut("{:3d} | tr: {:.3f} | vl: {:.3f}".format(i, loss, valloss / valcnt))
-        return valloss / valcnt
 
     class callback(LearnerCallback):
         def __init__(self, ln):
@@ -150,26 +136,8 @@ class Train(object):
             plt.legend()
             plt.savefig(plotpath)
 
-    def evaluations_bak(self, trainloader, valloader):
-        K = self.kwargs['nc']
-        with torch.no_grad():
-            self.net.eval()
-            for i, loader in enumerate((trainloader, valloader)):
-                Y = np.zeros(len(loader.dataset), dtype = np.int); Yhat = np.zeros(len(loader.dataset), dtype = np.int); idx = 0
-                for x, y in tqdm.tqdm(loader, desc = "Evaluating...", leave = False, mininterval = 60):
-                    x = x.cuda(); y = y.numpy()
-                    yhat = self.net(x).argmax(1).cpu().data.numpy()
-                    Y[idx:idx + len(y)] = y; Yhat[idx:idx + len(y)] = yhat; idx += len(y)
-                printOut(["Train", "Val"][i] + " kappa: %.4f" % (metrics.cohen_kappa_score(Y, Yhat, weights = "quadratic")))
-                printOut(["Train", "Val"][i] + ":\n" + str(metrics.confusion_matrix(Y, Yhat)))
-                printOut("\n~~~~~\n")
-        plt.plot(self.losses['train'], label = "train")
-        plt.plot(self.losses['val'], label = "val")
-        plt.legend()
-        plt.savefig(plotpath)
-
     def evaluations(self, ln):
-        pred,target = [],[]
+        pred, target = [],[]
         ln.model.eval()
         with torch.no_grad():
             for step, (x, y) in progress_bar(enumerate(ln.data.dl(DatasetType.Valid)), total=len(ln.data.dl(DatasetType.Valid))):
@@ -202,32 +170,15 @@ class Train(object):
         printOut("Val kappa: %.5f" % cohen_kappa_score(t, p, weights = 'quadratic'))
         printOut(confusion_matrix(t, p))
 
-    def train_bak(self):
-        trainloader, valloader = self.data.toLoader(self.kwargs['bs'])
-        for i in tqdm.tqdm(range(1, self.kwargs['ep'] + 1), desc = "Iterating...", mininterval = 60):
-            self.net.train()    
-            loss = 0; cnt = 0
-            for x, y in tqdm.tqdm(trainloader, desc = "Training...", leave = False, mininterval = 60):
-                x = x.cuda(); y = y.cuda()
-                yhat = self.net(x)
-                cost = self.loss(yhat, y.long())
-                loss += cost.item() * len(x); cnt += len(x)
-                self.opt.zero_grad()
-                cost.backward()
-                self.opt.step()
-            valloss = self.callback(i, loss / cnt, valloader)
-            self.sch.step(valloss)
-        torch.save(self.net.state_dict(), modelpath)
-        self.evaluations(trainloader, valloader)
-
     def train(self, dl = None):
-        dl = self.data.toLoader(self.kwargs['bs']) if dl is None else dl
-        ln = Learner(dl, self.net, loss_func = self.loss, opt_func = self.opt, metrics = [KappaScore(weights = 'quadratic')], bn_wd = False, wd = self.kwargs['wd']).to_fp16()
+        dl = self.data.get_data(self.kwargs['bs']) if dl is None else dl
+        # ln = Learner(dl, self.net, loss_func = self.loss, opt_func = self.opt, metrics = [KappaScore(weights = 'quadratic')], bn_wd = False, wd = self.kwargs['wd']).to_fp16()
+        ln = Learner(dl, self.net, loss_func = self.loss, opt_func = self.opt, metrics = [KappaScore(weights = 'quadratic')]).to_fp16()
         ln.clip_grad = 1.0
         ln.split([self.net.head])
         ln.unfreeze()
         cb = self.callback(ln)
-        ln.fit_one_cycle(self.kwargs['ep'], max_lr = self.kwargs['lr'], div_factor = self.kwargs['df'], pct_start = 0.0, wd = self.kwargs['wd'], callbacks = [cb])
+        ln.fit_one_cycle(self.kwargs['ep'], max_lr = self.kwargs['lr'], div_factor = self.kwargs['df'], pct_start = 0.0, callbacks = [cb])
         torch.save(self.net.state_dict(), modelpath)
         self.evaluations(ln)
 
